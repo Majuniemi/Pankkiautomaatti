8768e557bac9e8309b8d21410ef2035871833004
HannuKarjalainen <143908228+HannuKarjalainen@users.noreply.github.com>
2023-11-26 16:01:30 +0200

    clickhandlerpäivitys

---
+++ bank-automat/maininterface.h
@@ -0,0 +1,11 @@
+#ifndef MAININTERFACE_H
+#define MAININTERFACE_H
+#include "mainwindow.h"
+
+class maininterface:public MainWindow
+{
+public:
+    maininterface();
+};
+
+#endif // MAININTERFACE_H
