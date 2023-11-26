9cd52906e0316d4de64c6cedf34ac7ef1f4b28de
Matti Nieminen <m.nieminen2@gmail.com>
2023-11-26 20:28:53 +0200

    login toimii qtssa, mutta tunnusten syottäminen ei vielä valmis

--- bank-automat/mainwindow.h
+++ bank-automat/mainwindow.h
@@ -2,6 +2,9 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QtNetwork>
+#include <QNetworkAccessManager>
+#include <QJsonDocument>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -13,12 +16,20 @@ Q_OBJECT
 public:
 MainWindow(QWidget *parent = nullptr);
 ~MainWindow();
-int tila=2;
+int tila=0;
+
 public slots:
 void numberClickHandler();
 void commandClickHandler();
+void enterClickedHandler();
+
+private slots:
+void loginSlot(QNetworkReply *reply);
 
 private:
 Ui::MainWindow *ui;
+QNetworkAccessManager *postManager;
+QNetworkReply *reply;
+QByteArray response_data;
 };
 #endif // MAINWINDOW_H
