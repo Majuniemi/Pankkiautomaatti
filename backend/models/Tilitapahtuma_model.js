95b2b21bf6e2759d062b2188bc53214df729a124
Majuniemi <149418950+Majuniemi@users.noreply.github.com>
2023-12-10 23:18:04 +0200

    Tilitapahtuman model ja route lisätty

---
+++ backend/models/Tilitapahtuma_model.js
@@ -0,0 +1,28 @@
+const db = require('../database');
+
+const Tilitapahtumat = {
+  getAll: function(callback) {
+    return db.query('select * from Tilitapahtumat', callback);
+  },
+  getById: function(id, callback) {
+    return db.query('select * from Tilitapahtumat where idTilitapahtumat=?', [id], callback);
+  },
+  add: function(Tilitapahtumat, callback) {
+    return db.query(
+      'insert into Tilitapahtumat (Aikaleima, Saldomuutos, Muutoslaji, idTili, Paikkatieto) values(?,?,?,?,?)',
+      [Tilitapahtuma.Aikaleima, Tilitapahtuma.Saldomuutos, Tilitapahtuma.Muutoslaji, Tilitapahtuma.idTili, Tilitapahtuma.Paikkatieto],
+      callback
+    );
+  },
+  delete: function(id, callback) {
+    return db.query('delete from Tilitapahtumat where idTilitapahtumat=?', [id], callback);
+  },
+  update: function(id, Tilitapahtumat, callback) {
+    return db.query(
+      'update Tilitapahtumat set Aikaleima=?,Saldomuutos=?,Muutoslaji=?,idTili=?,Paikkatieto=?',
+      [Tilitapahtuma.Aikaleima, Tilitapahtuma.Saldomuutos, Tilitapahtuma.Muutoslaji, Tilitapahtuma.idTili, Tilitapahtuma.Paikkatieto, id],
+      callback
+    );
+  }
+};
+module.exports = Tilitapahtumat;
